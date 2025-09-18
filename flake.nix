b914c5c334f70266e2bf2d4f094e058339f81385
aster <137767097+aster-void@users.noreply.github.com>
2025-09-18 17:02:35 +0900

    init sveltekit

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-prisma-utils.url = "github:VanCoding/nix-prisma-utils";
+    nix-prisma-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
@@ -15,11 +17,7 @@
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            bun
-          ];
-        };
+        devShells.default = import ./nix/shell.nix {inherit pkgs inputs;};
       }
     );
 }
